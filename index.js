f9f1c377739ee5144611a1254c465bf20e6bb7af
Josh <grafton.josh@gmail.com>
2018-08-31 15:35:52 -0600

    Functioning version of pixel art

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ let palette = document.getElementsByTagName('div')[0]
 //Find currentColor
 let currentColor = document.getElementsByTagName('div')[4]
 
+//set
+let redSwatch = document.getElementsByTagName('div')[1]
+
+//This variable is used to choose the current color based on the palette color clicked. It needs to equal the color of the event.target. It should reference a css class containing only backgound or background color, otherwise it will insert the circle color from the palette. Use an if statement and the event.target name to set this variable
+let selectedColor = ''
+
 //add table to document within body
 body.appendChild(grid)
 
@@ -56,20 +62,25 @@ for (var i = 0; i < 20; i++) {
 
 }
 
-//Function to change class of event.target when a cell is clicked
+//Function to change color event.target when a cell is clicked
 var changeTargetBlue = function () {
-  event.target.classList.add('blue')
-  console.log('Click');
+  event.target.style.backgroundColor = selectedColor
+  console.log('Change table data function is working');
 }
 
 //Add addEventListener to the grid to detect event.target and change colors
 grid.addEventListener('click', changeTargetBlue);
 
-//Function to change class of currentColor
+//Function to change class of currentColor. Need to be able to choose the correct color to add as a parameter based on the event.target. Need to update selectedColor based on the event.target.
 var test = function () {
-  currentColor.classList.add('blue')
-  console.log('test function')
+  selectedColor = event.target.getAttribute('id')
+  currentColor.style.backgroundColor = selectedColor
+  console.log(`The selected color is ${selectedColor}`)
+  console.log('test function is working')
 }
 
 //Event listener to change current color to the same color as the palette swatch that was clicked
 palette.addEventListener('click', test)
+
+//document.getElementsByTagName('div')[4].style.backgroundColor = 'teal'
+//"teal" changes the currentColor to teal. write a function that looks at an event listener on the color palet and sets the color to the name of the color clicked. You coudld name each color and then use some "getElementByName" method as the color variable.
